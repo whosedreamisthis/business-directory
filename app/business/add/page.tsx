2b8b874383d7586eecdf07a22f3d244e97ad4940
Dana Sharon <whosedreamisthis@gmail.com>
2025-11-03 16:16:40 -0800

    more navigation (add business and hide dashboard if not logged in

---
+++ app/business/add/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AddBusinessPage() {
+	return (
+		<div className="flex justify-center items-center h-screen">
+			Add Business Page
+		</div>
+	);
+}
--- components/nav/top-nav.tsx
+++ components/nav/top-nav.tsx
@@ -13,8 +13,12 @@ import { Menu } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { LayoutDashboard, Plus, LogIn } from 'lucide-react';
+import DashboardLayout from '@/app/dashboard/layout';
+import { currentUser } from '@clerk/nextjs/server';
+export default async function TopNav() {
+	const user = await currentUser();
 
-export default function TopNav() {
 	return (
 		<Menubar>
 			<div className="flex-none">
@@ -33,11 +37,34 @@ export default function TopNav() {
 			<div className="flex grow items-center justify-end gap-1">
 				<MenubarMenu>
 					<MenubarTrigger className="text-base font-normal">
-						<Link href="/dashboard">Dashboard</Link>
+						<Link href="/business/add">
+							<span className="flex items-center">
+								<Plus size={16} className="mr-2" />
+								<span>Add Business</span>
+							</span>
+						</Link>
 					</MenubarTrigger>
 				</MenubarMenu>
+				{user && (
+					<MenubarMenu>
+						<MenubarTrigger className="text-base font-normal">
+							<Link href="/dashboard">
+								<span className="flex items-center">
+									<LayoutDashboard
+										size={16}
+										className="mr-2"
+									/>
+									<span>Dashboard</span>
+								</span>
+							</Link>
+						</MenubarTrigger>
+					</MenubarMenu>
+				)}
 				<SignedOut>
-					<SignInButton />
+					<span className="flex items-center">
+						<LogIn size={16} className="mr-2 inline" />
+						<SignInButton />
+					</span>
 				</SignedOut>
 				<SignedIn>
 					<UserButton />
